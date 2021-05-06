13b0644344cf5e3cfc6bf9119084a46ebf61d401
mink21 <minkyoung2112@gmail.com>
2021-05-06 22:31:41 +0900

    dfs practice

---
+++ iceCream.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n, m;
+bool graph[1000][1000];
+
+bool dfs(int x, int y)
+{
+    if (x < 0 || x >= n || y < 0 || y >= m)
+        return false;
+    if (!graph[x][y])
+    {
+        graph[x][y] = true;
+        dfs(x - 1, y);
+        dfs(x + 1, y);
+        dfs(x, y - 1);
+        dfs(x, y + 1);
+        return true;
+    }
+    return false;
+}
+
+void print_arr()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (graph[i][j])
+                cout << "1 ";
+            else
+                cout << "0 ";
+        }
+    }
+}
+
+int main(void)
+{
+    cin >> n >> m;
+    string str;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str;
+        for (int j = 0; j < m; j++)
+        {
+            if (str[j] == 0)
+                graph[i][j] = true;
+        }
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (dfs(i, j))
+                result++;
+        }
+    }
+
+    cout << result << "\n";
+    return 0;
+}
