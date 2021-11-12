007ea63e8ecd666ea2e8a1f683697c4d9375cc6c
mink21 <minkyoung2112@gmail.com>
2021-11-12 15:22:01 +0900

    math

--- baekjoon/1978.cpp
+++ baekjoon/1978.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cmath>
 
+#define MAX 1000+1
 using namespace std;
 
 int n;
-bool isPrime[1001];
+bool isPrime[MAX];
 
 void checkIsPrime(){
-    for(int i = 2; i<1001; i++){
+    for(int i = 2; i<MAX; i++){
         isPrime[i] = 1;
     }
-    int sqrtn = sqrt(n);
+    int sqrtn = sqrt(MAX);
     for(int i = 2; i<=sqrtn; i++){
         if(!isPrime[i]) continue;
         for(int j = i+i; j<=n; j += i){
