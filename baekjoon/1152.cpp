7ccd620ba783ae6dde75ec8999f0fd34a8066ee4
mink21 <minkyoung2112@gmail.com>
2021-11-13 16:43:29 +0900

    string practice c++

--- baekjoon/1152.cpp
+++ baekjoon/1152.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
+void solve(void)
+{
+    string s;
+    getline(cin, s);
+
+    int cnt = 0;
+    bool flag = true;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            flag = true;
+        }
+        else if (flag)
+        {
+            flag = false;
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
 
 int main(void)
 {
-    string S;
-    getline(cin, S);
-    int count = 1;
-    for (int i = 1; i < S.length() - 1; i++)
-    {
-        if (S[i] == ' ')
-        {
-            count++;
-        }
-    }
-    cout << count << endl;
+    solve();
     return 0;
 }
