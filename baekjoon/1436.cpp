6b06181e10535f7f5717cd69635548630c0a3090
mink21 <minkyoung2112@gmail.com>
2021-11-25 16:47:55 +0900

    brute force, math, sorting

--- baekjoon/1436.cpp
+++ baekjoon/1436.cpp
@@ -1,1 +1,31 @@
-#in
+#include <iostream>
+
+using namespace std;
+
+void solve(void){
+    int n;
+    cin>>n;
+
+    int num = 666;
+    int cnt = 0;
+    while(true){
+        int temp = num;
+        while(temp>0){
+            if(temp%1000 ==666){
+                cnt++;
+                break;
+            }
+            temp/=10;
+        }
+        if(cnt == n){
+            cout<<num<<endl;
+            break;
+        }
+        num++;
+    }
+}
+
+int main(void){
+    solve();
+    return 0;
+}
