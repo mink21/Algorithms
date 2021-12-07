78e227ca9909bb0cfb466dcafa64a5803c4a0ecf
mink21 <minkyoung2112@gmail.com>
2021-12-07 14:42:18 +0900

    advent of code day 7

---
+++ 2021AdventOfCode/7_2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int crab[2000] = {0};
+int maxHorizontal = 0;
+
+void readInput(void)
+{
+    string s;
+    getline(cin, s);
+
+    string delimiter = ",";
+    size_t pos = 0;
+    string token;
+    while ((pos = s.find(delimiter)) != string::npos)
+    {
+        token = s.substr(0, pos);
+        int temp = stoi(token);
+        if (temp > maxHorizontal)
+            maxHorizontal = temp;
+        crab[temp]++;
+        s.erase(0, pos + delimiter.length());
+    }
+    int temp = stoi(s);
+    if (temp > maxHorizontal)
+        maxHorizontal = temp;
+    crab[temp]++;
+}
+
+int calculateCost(int pos)
+{
+    int cost = 0;
+    for (int i = 0; i < maxHorizontal + 1; i++)
+    {
+        for (int j = 0; j < crab[i]; j++)
+        {
+            int n = abs(i - pos);
+            cost += n * (n + 1) / 2;
+        }
+    }
+    return cost;
+}
+
+void solve(void)
+{
+    for (int i = 0; i < 5; i++)
+        readInput();
+    int minCost = 1000000000;
+    for (int i = 0; i < maxHorizontal + 1; i++)
+    {
+        int temp = calculateCost(i);
+        // cout << i << " " << temp << endl;
+        if (temp < minCost)
+            minCost = temp;
+    }
+    cout << minCost << endl;
+}
+
+int main(void)
+{
+    solve();
+    return 0;
+}
